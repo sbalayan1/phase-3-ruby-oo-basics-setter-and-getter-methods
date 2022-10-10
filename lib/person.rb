ed3e2d9023ce42fb955b7f89edaa43285f227458
sbalayan1 <balayans2014@yahoo.com>
2022-10-09 20:10:54 -0700

    update

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
+    def name=(dog_name)
+        @name = dog_name
+    end
+    def name
+        @name
+    end
 
+    def job=(dog_job)
+        @job = dog_job
+    end
+    def job
+        @job
+    end
 end
